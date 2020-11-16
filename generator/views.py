f4d2f3c905af37e9e6dd821c12bc23ac1e273595
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-11-16 14:21:21 +0100

    del login

--- generator/views.py
+++ generator/views.py
@@ -2,25 +2,6 @@ from django.shortcuts import render, redirect
 from .models import DataSchemas
 from django.contrib.auth.forms import UserCreationForm
 
-
-# def login(request):
-#     if request.method == 'POST':
-#         login_form = LoginForm(request.POST)
-#         if login_form.is_valid():
-#             user_data = User()
-#             user_data.email = login_form.data.get("email", None)
-#             user_data.password = login_form.data.get("password", None)
-#             user_data.save()
-#             user = User.objects.filter(email=user_data.email, password=user_data.password).first()
-#             if user is not None:
-#                 request.session['user_id'] = user.id
-#                 username = user.email
-#                 return redirect('schemas')
-#             return render(request, 'registration/login.html')
-#         else:
-#             return render(request, 'registration/login.html')
-#     return render(request, 'registration/login.html')
-
 def home(request):
     return render(request, 'home.html')
 
