3e0ac8d27078b84245c0d8fa6210846a17003dbf
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-11-17 11:44:35 +0100

    del ColumnNewSchema

--- generator/models.py
+++ generator/models.py
@@ -3,28 +3,17 @@ from datetime import datetime
 
 
 class NewSchema(models.Model):
+
     schema_name = models.CharField(max_length=30, verbose_name='Schema name', unique=True)
     column_separator = models.CharField(max_length=30, verbose_name='Column separator')
     string_character = models.CharField(max_length=30, verbose_name='String character')
+    user_id = models.CharField(max_length=30, verbose_name='User id')
+    date = models.DateTimeField(default=datetime.now, blank=True)
 
-class ColumnNewSchema(models.Model):
-    TYPE_LIST = [
-        ('FN', 'Full name'),
-        ('E', 'Email'),
-        ('DN', 'Domain name'),
-        ('PN', 'Phone number'),
-        ('CN', 'Company name'),
-        ('T', 'Text'),
-        ('A', 'Address'),
-        ('D', 'Date'),
-    ]
-    schema_id = models.ForeignKey(NewSchema, to_field="id", on_delete=models.CASCADE)
-    column_name = models.CharField(max_length=30, verbose_name='Column name')
-    type_name = models.CharField(max_length=30, choices=TYPE_LIST, verbose_name='Type name')
-    from_field = models.CharField(max_length=10, verbose_name='From')
-    to_field = models.CharField(max_length=10, verbose_name='To')
-    order = models.CharField(max_length=10, verbose_name='Order')
+class SetSchema(models.Model):
 
-class DataSchemas(models.Model):
-    name = models.ForeignKey(NewSchema, to_field="schema_name", on_delete=models.CASCADE)
+    schema_id = models.ForeignKey(NewSchema, to_field="id", on_delete=models.CASCADE, default=True)
+    column_name = models.CharField(max_length=30, verbose_name='Column name', default=True)
+    column_type = models.CharField(max_length=30, verbose_name='Column type', default=True)
+    column_order = models.CharField(max_length=30, verbose_name='Column order', default=True)
     date = models.DateTimeField(default=datetime.now, blank=True)
