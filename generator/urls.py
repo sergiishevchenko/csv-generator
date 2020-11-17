a14ac0d711c26870ca503c4bb94eda3f6d619d3e
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-11-17 11:45:52 +0100

    button delete

--- generator/urls.py
+++ generator/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from generator.views import home, schemas, new_schema, edit_schema, signup
+from generator.views import home, schemas, new_schema, edit_schema, signup, del_schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('schemas/', schemas, name='schemas'),
     path('schemas/new_schema', new_schema, name='new_schema'),
-    path('schemas/edit_schema', edit_schema, name='edit_schema')
+    path('schemas/edit_schema', edit_schema, name='edit_schema'),
+    path('schemas/<id>', del_schema, name='del_schema')
 ]
