abcf6ddacc82c532a01e11476083757bba88e6b0
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-11-18 17:24:12 +0100

    added Sets

--- generator/models.py
+++ generator/models.py
@@ -17,7 +17,7 @@ class NewSchema(models.Model):
     column_separator = models.CharField(max_length=30, choices=Separator, verbose_name='Column separator')
     string_character = models.CharField(max_length=30, choices=StringCharacter, verbose_name='String character')
     user_id = models.CharField(max_length=30, verbose_name='User id')
-    date = models.DateTimeField(default=datetime.now, blank=True)
+    date = models.CharField(max_length=100, default=datetime.now().strftime("%m/%d/%Y-%H:%M:%S"), blank=True)
 
     def __str__(self):
         return self.schema_name
@@ -29,12 +29,21 @@ class SetSchema(models.Model):
     column_name = models.CharField(max_length=30, verbose_name='Column name', default=True)
     column_type = models.CharField(max_length=30, verbose_name='Column type', default=True)
     column_order = models.CharField(max_length=30, verbose_name='Column order', default=True)
-    date = models.DateTimeField(default=datetime.now, blank=True)
+    date = models.CharField(max_length=100, default=datetime.now().strftime("%m/%d/%Y-%H:%M:%S"), blank=True)
     set_schema = models.CharField(max_length=30, verbose_name='Name of schema', default=True)
-    # id_set = 
 
     def __str__(self):
         return self.column_name
 
     class Meta:
         db_table = "SetSchema"
+
+class Sets(models.Model):
+    schema_name = models.CharField(max_length=30, verbose_name='Schema name', default=True)
+    data = models.CharField(max_length=100, default=datetime.now().strftime("%m/%d/%Y-%H:%M:%S"), blank=True)
+
+    def __str__(self):
+        return self.schema_name
+
+    class Meta:
+        db_table = "Sets"
