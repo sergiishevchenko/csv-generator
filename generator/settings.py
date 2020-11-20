f9fc6799039670d067e3cb02083b44bed848a0af
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-11-20 11:10:07 +0100

    added logging

--- generator/settings.py
+++ generator/settings.py
@@ -104,6 +104,22 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
